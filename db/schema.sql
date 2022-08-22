fe2b581abca292d8f4bd4074e5fa183c06e31552
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-21 21:06:25 -0400

    start populating tables

--- db/schema.sql
+++ db/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -24,5 +25,32 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) on DELETE SET NULL
 );
+
+INSERT INTO department (name)
+    VALUES
+        ('Sales'),
+        ('Estimating'),
+        ('Engineering'),
+        ('Accounting'),
+        ('Project Management'),
+        ('Logistics');
+
+INSERT INTO role (title, salary, department_id)
+    VALUES
+        ('Salesperson'),
+        ('Estimator'),
+        ('Senior Engineer'),
+        ('Engineer'),
+        ('Programmer'),
+        ('Senior Accountant'),
+        ('Accountant'),
+        ('Project Manager'),
+        ('Project Coordinator');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES
+        ('')
