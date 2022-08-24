15a3b7729304a86c3ecb8dd165fd3a370b4c4628
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-24 17:11:49 -0400

    added queries for add employee/department/update role

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 // const db = require('./db/connection');
-const Department = require('./lib/Department');
+const Departments = require('./lib/Department');
 const Employees = require('./lib/Employee');
-const Role = require('./lib/Role');
+const Roles = require('./lib/Role');
 const team = [];
 
 require('console.table');
@@ -56,9 +56,167 @@ const promptMenu = () => {
         })
 };
 
-const viewEmployees= async() => {
+const viewDepartments = async() => {
+    const [departments] = await Departments.getAllDepartments();
+    console.table(departments);
+};
+
+const viewRoles = async() => {
+    const [roles] = await Roles.getAllRoles();
+    console.table(roles);
+};
+
+const viewEmployees = async() => {
     const [employees] = await Employees.getAllEmployees();
     console.table(employees);
+};
+
+const addDepartment = () => {
+    console.log(`
+    ================
+    Add a Department
+    ================
+    `)
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: "What is the name of the new department?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the name of the new department!');
+                        return false;
+                    }
+                }
+            }
+        ])
+};
+
+const addRole = () => {
+    console.log(`
+    ===========
+    Add a Role
+    ===========
+    `)
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'role',
+                message: "What is the new role you'd like to add?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the name of the new role!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: "What is the salary for this new role?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the salary for this new role!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'departmentId',
+                message: "Please choose a department for the new role",
+                choices: [{
+                    //department id data from departments table
+                }],
+            }
+        ])
+}
+
+const addEmployee = () => {
+    console.log(`
+    ===============
+    Add an Employee
+    ===============
+    `)
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the employee's name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the employee name');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "What is the employee's last name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide the last name of the new employee');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "Please select a role for the new employee.",
+                choices: [{
+                    // insert roles table data
+                }]
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: "Please select a manager for the new employee.",
+                choices: [{
+                    // insert managers (or supervisors/superiors) data from employees table
+                }]
+            }
+        ])
+}
+
+const updateEmployeeRole = () => {
+    console.log(`
+    ==========================
+    Update an Employee's Role
+    ==========================
+    `)
+
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employees',
+                message: "Please select an employee to update.",
+                choices: [{
+                    // insert employee table data
+                }]
+            },
+            {
+                type: ''
+            }
+        ])
 }
 
 promptMenu()
